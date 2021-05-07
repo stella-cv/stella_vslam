da675e0c3f626c22586c9133afc0ec1a2c88e067
ymd-stella <world.applepie@gmail.com>
2021-05-07 13:51:38 +0900

    Fix operator<< for config

--- src/openvslam/config.cc
+++ src/openvslam/config.cc
@@ -119,7 +119,7 @@ config::~config() {
 
 std::ostream& operator<<(std::ostream& os, const config& cfg) {
     os << "Camera Configuration:" << std::endl;
-    os << cfg.camera_;
+    os << *cfg.camera_;
     os << std::endl;
 
     os << "ORB Configuration:" << std::endl;
