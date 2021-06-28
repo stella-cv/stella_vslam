38307c5b1b4b283c2c17acea58d0101ae206e983
Masahiro Ogawa <m-ogawa@sensyn-robotics.com>
2021-06-28 16:31:03 +0900

    update README.md file name.

--- scripts/ubuntu/download_sampledata.sh
+++ scripts/ubuntu/download_sampledata.sh
@@ -22,7 +22,6 @@ mkdir $TOPDIR/vocab
     curl -sLb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${FILE_ID}" -o aist_living_lab_1.zip
     unzip aist_living_lab_1.zip
     
-# download a sample dataset from Google Drive
     FILE_ID="1TVf2D2QvMZPHsFoTb7HNxbXclPoFMGLX"
     curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
     CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
