e9755cdbfbf0c58ffd7f0b9330ca181984f0749d
stella <stella@stella>
2022-07-18 13:15:41 +0900

    Start from specified time

--- example/run_video_slam.cc
+++ example/run_video_slam.cc
@@ -33,6 +33,7 @@ void mono_tracking(const std::shared_ptr<stella_vslam::config>& cfg,
                    const std::string& video_file_path,
                    const std::string& mask_img_path,
                    const unsigned int frame_skip,
+                   const unsigned int start_time,
                    const bool no_sleep,
                    const bool wait_loop_ba,
                    const bool auto_term,
@@ -67,6 +68,11 @@ void mono_tracking(const std::shared_ptr<stella_vslam::config>& cfg,
 #endif
 
     auto video = cv::VideoCapture(video_file_path, cv::CAP_FFMPEG);
+    if (!video.isOpened()) {
+        std::cerr << "Unable to open the video." << std::endl;
+        return;
+    }
+    video.set(0, start_time);
     std::vector<double> track_times;
 
     cv::Mat frame;
@@ -185,6 +191,7 @@ int main(int argc, char* argv[]) {
     auto config_file_path = op.add<popl::Value<std::string>>("c", "config", "config file path");
     auto mask_img_path = op.add<popl::Value<std::string>>("", "mask", "mask image path", "");
     auto frame_skip = op.add<popl::Value<unsigned int>>("", "frame-skip", "interval of frame skip", 1);
+    auto start_time = op.add<popl::Value<unsigned int>>("s", "start-time", "time to start playing [milli seconds]", 0);
     auto no_sleep = op.add<popl::Switch>("", "no-sleep", "not wait for next frame in real time");
     auto wait_loop_ba = op.add<popl::Switch>("", "wait-loop-ba", "wait until the loop BA is finished");
     auto auto_term = op.add<popl::Switch>("", "auto-term", "automatically terminate the viewer");
@@ -240,6 +247,7 @@ int main(int argc, char* argv[]) {
                       video_file_path->value(),
                       mask_img_path->value(),
                       frame_skip->value(),
+                      start_time->value(),
                       no_sleep->is_set(),
                       wait_loop_ba->is_set(),
                       auto_term->is_set(),
