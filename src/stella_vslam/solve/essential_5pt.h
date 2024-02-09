269cbc513354a913723ddafada098453ef5a5a02
jjd9 <jjdang21@gmail.com>
2024-02-09 09:45:13 -0600

    fix typo

--- src/stella_vslam/solve/essential_5pt.h
+++ src/stella_vslam/solve/essential_5pt.h
@@ -37,7 +37,7 @@ namespace stella_vslam {
 // some special sized eigen matrices we only use here
 using Mat10_t = Eigen::Matrix<double, 10, 10>;
 
-using Vec20_t = Vec20_t;
+using Vec20_t = Eigen::Matrix<double, 1, 20>;
 
 // Polynomial coefficients
 // slightly different from the paper, which seems to have a typo here
@@ -96,7 +96,7 @@ MatX_t find_nullspace_of_epipolar_constraint(const eigen_alloc_vector<Vec3_t>& x
  * using GrLex order
  * [... xx xy yy xz yz zz x y z 1]
 */
-Vec20_t deg_one_coeff_product(const Vec20_t& poly1, const Vec20_t& poly2) {
+Vec20_t deg_one_poly_product(const Vec20_t& poly1, const Vec20_t& poly2) {
     Vec20_t product = Vec20_t::Zero();
 
     product(coeff_xx) = poly1(coeff_x) * poly2(coeff_x); // x*x'
@@ -118,7 +118,7 @@ Vec20_t deg_one_coeff_product(const Vec20_t& poly1, const Vec20_t& poly2) {
  * Multiply a 2 deg poly, poly1 and a one deg poly, poly2 (in x, y, z) using GrLex order
  * [xxx xxy xyy yyy xxz xyz yyz xzz yzz zzz xx xy yy xz yz zz x y z 1]
 */
-Vec20_t deg_two_coeff_product(const VecX_t& poly1, const VecX_t& poly2) {
+Vec20_t deg_two_poly_product(const VecX_t& poly1, const VecX_t& poly2) {
     Vec20_t product(20);
 
     product(coeff_xxx) = poly1(coeff_xx) * poly2(coeff_x);
@@ -183,20 +183,21 @@ Eigen::Matrix<double, 10, 20> form_polynomial_constraint_matrix(const Eigen::Mat
     int mrow = 0;
 
     // Theorem 1: Determinant constraint det(E) = 0 is the first part of M
-    M.row(mrow++) = (deg_two_coeff_product(deg_one_coeff_product(E[0][1], E[1][2]) - deg_one_coeff_product(E[0][2], E[1][1]), E[2][0]) + 
-                    deg_two_coeff_product(deg_one_coeff_product(E[0][2], E[1][0]) - deg_one_coeff_product(E[0][0], E[1][2]), E[2][1]) + 
-                    deg_two_coeff_product(deg_one_coeff_product(E[0][0], E[1][1]) - deg_one_coeff_product(E[0][1], E[1][0]), E[2][2]));
+    M.row(mrow++) = (deg_two_poly_product(deg_one_poly_product(E[0][1], E[1][2]) - deg_one_poly_product(E[0][2], E[1][1]), E[2][0]) + 
+                    deg_two_poly_product(deg_one_poly_product(E[0][2], E[1][0]) - deg_one_poly_product(E[0][0], E[1][2]), E[2][1]) + 
+                    deg_two_poly_product(deg_one_poly_product(E[0][0], E[1][1]) - deg_one_poly_product(E[0][1], E[1][0]), E[2][2]));
 
     // Theorem 2: the trace constraint: EEtE - 1/2 trace(EEt)E = 0
+    // is the rest of M
 
     // EEt == E * E.transpose()
     Vec20_t EET[3][3];
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             if (i <= j) {
-                EET[i][j] = deg_one_coeff_product(E[i][0], E[j][0])
-                            + deg_one_coeff_product(E[i][1], E[j][1])
-                            + deg_one_coeff_product(E[i][2], E[j][2]);
+                EET[i][j] = deg_one_poly_product(E[i][0], E[j][0])
+                            + deg_one_poly_product(E[i][1], E[j][1])
+                            + deg_one_poly_product(E[i][2], E[j][2]);
             }
             else {
                 // EET is symmetric
@@ -215,9 +216,9 @@ Eigen::Matrix<double, 10, 20> form_polynomial_constraint_matrix(const Eigen::Mat
     // (EEt - 1/2 trace(EEt)) * E --> EEtE - 1/2 trace(EEt)E = 0
     for (const int i : {0, 1, 2}) {
         for (const int j : {0, 1, 2}) {
-            M.row(mrow++) = deg_two_coeff_product(trace_constraint[i][0], E[0][j])
-                            + deg_two_coeff_product(trace_constraint[i][1], E[1][j])
-                            + deg_two_coeff_product(trace_constraint[i][2], E[2][j]);
+            M.row(mrow++) = deg_two_poly_product(trace_constraint[i][0], E[0][j])
+                            + deg_two_poly_product(trace_constraint[i][1], E[1][j])
+                            + deg_two_poly_product(trace_constraint[i][2], E[2][j]);
         }
     }
 
